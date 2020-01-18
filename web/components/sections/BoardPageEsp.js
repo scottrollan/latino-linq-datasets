c9ddf378aa5239b83e609c3b5df93dd2d2537b07
Barry Rollan <barry.rollan@gmail.com>
2020-01-18 09:52:14 -0500

    forgot, so added componentDidMount feature to spanish board page

--- web/components/sections/BoardPageEsp.js
+++ web/components/sections/BoardPageEsp.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
+import styles from './BoardPage.module.css'
 
-class BoardPageEsp extends Component {
+class BoardPage extends Component {
   state = {
     boardMembers: []
   }
-  render () {
-    let boardMembers = []
+  componentDidMount () {
     const sanityClient = require('@sanity/client')
     const client = sanityClient({
       projectId: 'q4pr99l8',
@@ -14,25 +14,24 @@ class BoardPageEsp extends Component {
       useCdn: true // `false` if you want to ensure fresh data
     })
     const query = `*[_type=='board'] | order(displayOrder asc)`
-    client.fetch(query).then(member => {
-      member.forEach(m => {
-        boardMembers.push(m)
-      })
-      this.setState({boardMembers: boardMembers})
+    client.fetch(query).then(response => {
+      this.setState({boardMembers: response})
     })
-
+  }
+  render () {
     return (
       <section>
         {this.state.boardMembers.map((person, index) => {
           let imageArray = person.memberImage.asset._ref.split('-') // splits _ref into an array of length 4
           let imageUrl = `https://cdn.sanity.io/images/q4pr99l8/production/${imageArray[1]}-${imageArray[2]}.${imageArray[3]}?h=200&fit=max` // gives image <image id>-<original size>.<extension>
           return (
-            <div key={index} style={{textAlign: 'justify', minHeight: '280px', margin: '0', padding: '30px 20%', background: person.displayOrder % 2 === 0 ? 'white' : 'black'}}
+            <div key={index} className={styles.memberDiv} style={{background: person.displayOrder % 2 === 0 ? 'white' : 'black'}}
             >
               <img
                 src={imageUrl}
                 alt=''
-                style={{margin: person.displayOrder % 2 === 0 ? '0 0 10px 20px' : '0 20px 10px 0', float: person.displayOrder % 2 === 0 ? 'right' : 'left'}}
+                className={styles.foto}
+                style={{float: person.displayOrder % 2 === 0 ? 'right' : 'left', margin: person.displayOrder % 2 === 0 ? '0 0 2px 10px' : '0 10px 2px 0'}}
               />
               <h3 style={{color: person.displayOrder % 2 === 0 ? 'black' : 'white'}}>{person.name}, {person.titleEsp}</h3>
               <p style={{color: person.displayOrder % 2 === 0 ? 'black' : 'white'}}>{person.bioEsp}</p>
@@ -44,4 +43,4 @@ class BoardPageEsp extends Component {
   }
 }
 
-export default BoardPageEsp
+export default BoardPage
